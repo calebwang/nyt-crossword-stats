517c0f0443923a47f912190f6f5be22511a44328
caleb <caleb@asana.com>
2020-09-05 15:24:28 -0700

    Updates

---
+++ precache-manifest.4cb40573410f8459e7339ac6e5464287.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6c4b91c183f64b5d5f2bf5f395f360",
+    "url": "/nyt_crossword_stats/index.html"
+  },
+  {
+    "revision": "981df6666860856a1556",
+    "url": "/nyt_crossword_stats/static/css/2.5ed3c533.chunk.css"
+  },
+  {
+    "revision": "d16cea18ca1ab35af1d2",
+    "url": "/nyt_crossword_stats/static/css/main.b5ba778c.chunk.css"
+  },
+  {
+    "revision": "981df6666860856a1556",
+    "url": "/nyt_crossword_stats/static/js/2.1d91d1a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nyt_crossword_stats/static/js/2.1d91d1a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16cea18ca1ab35af1d2",
+    "url": "/nyt_crossword_stats/static/js/main.dd341e5a.chunk.js"
+  },
+  {
+    "revision": "f32133a465d252a6e6fb",
+    "url": "/nyt_crossword_stats/static/js/runtime-main.24a000a1.js"
+  }
+]);
