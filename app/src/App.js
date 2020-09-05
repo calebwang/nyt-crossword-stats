2a8332c6154a5dce2b714670c8bbe4e30c098c38
caleb <caleb@asana.com>
2020-09-04 19:44:30 -0700

    Implement puzzle loading and processing

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { interpolate, monthsRange, addMonths, monthStartAndEnd, formatDate } from "./utils.js";
+import { interpolate, monthsRange, addMonths, monthStartAndEnd, formatDate, RequestPool } from "./utils.js";
 import dc from "dc";
 
 
@@ -116,7 +116,7 @@ class DataContent extends React.Component {
 
     render() {
         return (
-            <DataLoader userId={this.props.userId} render={this.renderVisualization} />
+            <DataLoader userId={this.props.userId} userCookie={this.props.userCookie} render={this.renderVisualization} />
         );
     }
 
@@ -135,7 +135,8 @@ class DataLoader extends React.Component {
         this.data = {};
         this.state = {
             loaded: false,
-            numPendingRequests: 0,
+            data: {},
+            progress: "",
         };
     }
 
@@ -153,55 +154,165 @@ class DataLoader extends React.Component {
 
     render() {
         if (this.state.loaded === false) {
-            return "Loading foo";
+            return "Loading " + this.state.progress;
         }
 
-        return this.props.render(this.data);
+        return this.props.render(this.state.data);
     }
 
     fetch() {
         const now = new Date();
         const [currentYear, currentMonth] = [now.getFullYear(), now.getMonth()];
-        const [startYear, startMonth] = addMonths(currentYear, currentMonth, -12);
+        const [startYear, startMonth] = addMonths(currentYear, currentMonth, -1);
         const months = monthsRange(startYear, startMonth, currentYear, currentMonth);
-        console.log(months);
+
+        const monthDataRequestPool = new RequestPool(
+            20,
+            (numCompleted, numTotal) => {
+                this.setState({
+                    progress: numCompleted + "/" + numTotal + " months"
+                });
+            },
+        );
+
         months.forEach(yearAndMonth => {
             const [year, month] = yearAndMonth;
-            this.fetchMonth(year, month);
+            monthDataRequestPool.addRequest(() => this.fetchMonth(year, month));
         });
-        this.setState({
-            numPendingRequests: months.length,
+        monthDataRequestPool.start();
+        monthDataRequestPool.poll().then(results => {
+            results.forEach(result => this.processMonthData(result));
+            this.setState({
+                progress: "puzzle data",
+            });
+            this.fetchPuzzles();
         });
     }
 
     fetchMonth(year, month) {
         const [monthStart, monthEnd] = monthStartAndEnd(year, month);
-        window.fetch(
+        return window.fetch(
             interpolate(DataLoader.ARCHIVE_URL, {
                 uid: this.props.userId,
                 start_date: formatDate(monthStart),
                 end_date: formatDate(monthEnd),
             })
         )
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            data.results.forEach(puzzleData => {
-                this.data[puzzleData.print_date] = puzzleData;
+            .then(response => response.json());
+    }
+
+    processMonthData(data) {
+        console.log(data);
+        data.results.forEach(puzzleData => {
+            this.data[puzzleData.print_date] = puzzleData;
+        });
+    }
+
+    fetchPuzzles() {
+        console.log("fetching puzzles");
+        const puzzleRequestPool = new RequestPool(
+            20,
+            (numCompleted, numTotal) => {
+                this.setState({
+                    progress: numCompleted + "/" + numTotal + " puzzles"
+                });
+            },
+        )
+        Object.keys(this.data).forEach(puzzleDate => { // Optimization: Skip fetching puzzles if they haven't been filled out at all.
+            // Should work fine with the Puzzle class but may wrongly classify puzzles
+            // as unattempted (if attempted but not filled out at all)
+            if (this.data[puzzleDate].percent_filled !== 0) {
+                puzzleRequestPool.addRequest(() => this.fetchPuzzle(puzzleDate));
+            }
+        });
+
+        puzzleRequestPool.start();
+        puzzleRequestPool.poll().then(results => {
+            results.forEach(result => {
+                const [puzzleDate, solveData] = result;
+                this.data[puzzleDate].calcs = solveData.calcs;
+                this.data[puzzleDate].firsts = solveData.firsts;
             });
-            this.setState(state => {
-                return {
-                    numPendingRequests: state.numPendingRequests - 1,
-                };
+
+            const processedData = Object.values(this.data).map(puzzleData => new Puzzle(puzzleData).blob());
+
+            this.setState({
+                loaded: true,
+                data: processedData,
             });
         });
     }
 
+    fetchPuzzle(puzzleDate) {
+        const puzzleId = this.data[puzzleDate].puzzle_id;
+        console.log(puzzleId);
+        return window.fetch(
+            interpolate(DataLoader.GAME_URL, {
+                gid: puzzleId,
+            }), {
+                headers: {
+                    "nyt-s": this.props.userCookie,
+                }
+            }
+        )
+            .then(response => response.json())
+            .then(json => [puzzleDate, json]);
+
+    }
 }
 
 class DataVisualizer extends React.Component {
     render() {
         return JSON.stringify(this.props.data, null, 4);
+    }
+}
+
+class Puzzle {
+    constructor(data) {
+        this.data = data;
+    }
+
+    blob() {
+        return {
+            id: this.id(),
+            date: this.date(),
+            day: this.day(),
+            attempted: this.attempted(),
+            solved: this.solved(),
+            cleanlySolved: this.cleanlySolved(),
+            solveTime: this.solveTime(),
+        };
+    }
+
+    id() {
+        return this.data.puzzle_id;
+    }
+
+    date() {
+        return this.data.print_date;
+    }
+
+    day() {
+        return new Date(this.data.print_date).getUTCDay();
+    }
+
+    attempted() {
+        return this.data.firsts !== undefined;
+    }
+
+    solved() {
+        return this.data.solved;
+    }
+
+    cleanlySolved() {
+        return this.solved() && this.data.firsts.checked === undefined;
+    }
+
+    solveTime() {
+        if (this.solved()) {
+            return this.data.calcs.secondsSpentSolving;
+        }
+        return -1;
     }
 }
 
