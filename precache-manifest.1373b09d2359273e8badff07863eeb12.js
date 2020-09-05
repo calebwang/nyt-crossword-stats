3679608154a6a9b76d2f739b9205a79e719dc58c
caleb <caleb@asana.com>
2020-09-05 15:27:04 -0700

    Updates

---
+++ precache-manifest.1373b09d2359273e8badff07863eeb12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a74fe3aa4afaec58507883f4e910ca",
+    "url": "/nyt-crossword-stats/index.html"
+  },
+  {
+    "revision": "981df6666860856a1556",
+    "url": "/nyt-crossword-stats/static/css/2.5ed3c533.chunk.css"
+  },
+  {
+    "revision": "0ea634eff6faf932b4b2",
+    "url": "/nyt-crossword-stats/static/css/main.b5ba778c.chunk.css"
+  },
+  {
+    "revision": "981df6666860856a1556",
+    "url": "/nyt-crossword-stats/static/js/2.1d91d1a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nyt-crossword-stats/static/js/2.1d91d1a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea634eff6faf932b4b2",
+    "url": "/nyt-crossword-stats/static/js/main.5c39b219.chunk.js"
+  },
+  {
+    "revision": "87edd73e16cd2aba1f4f",
+    "url": "/nyt-crossword-stats/static/js/runtime-main.076c0cad.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nyt_crossword_stats/precache-manifest.4cb40573410f8459e7339ac6e5464287.js"
+  "/nyt-crossword-stats/precache-manifest.1373b09d2359273e8badff07863eeb12.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nyt_crossword_stats/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nyt-crossword-stats/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
