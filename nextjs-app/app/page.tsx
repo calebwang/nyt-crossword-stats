c483d14517708bdd4a52c3b6e2e98d5c4c407482
caleb <caleb@caleb.wang>
2023-08-02 22:21:42 -0700

    first pass at converting home page

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 export default function Home() {
   return (
@@ -9,6 +10,30 @@ export default function Home() {
 }
 
 function UserForm() {
+    const router = useRouter();
+    const [userId, setUserId] = useState(null);
+    const [userCookie, setUserCookie] = useState(null);
+    const [dateRangeOption, setDateRangeOption] = useState(null);
+
+    const submit = () => {
+        router.push({
+            pathname: '/visualize',
+            query: {q: `userId=${userId}&userCookie=${userCookie}&defaultDateRange=${dateRangeOption}`},
+       });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        submit();
+    };
+
+    const handleFormKeyPress = (e) => {
+        if (e.charCode === 13) {
+            submit();
+            e.preventDefault();
+        }
+    };
+
     return (
         <div className="UserForm">
             <div className="UserForm-header">
@@ -37,25 +62,24 @@ function UserForm() {
                     </li>
                 </ol>
             </div>
-            <form className="UserForm-form" onSubmit={this.handleSubmit}> <div className="UserForm-fields">
+            <form className="UserForm-form" onSubmit={handleSubmit}> <div className="UserForm-fields">
                     <div id="UserForm-userId" className="UserForm-field">
                         <label className="UserForm-label">User ID</label>
-                        <input className="UserForm-fieldInput" id="UserForm-userIdInput" type="text" value={this.state.userId} onChange={this.setUserId} />
+                        <input className="UserForm-fieldInput" id="UserForm-userIdInput" type="text" value={userId} onChange={setUserId} />
                     </div>
                     <div id="UserForm-dateRange" className="UserForm-field">
                         <label className="UserForm-label">Date Range</label>
-                        <select name="dateRange" id="UserForm-dateRangeInput" className="UserForm-fieldInput" value={this.state.dateRangeOption} onChange={this.setDateRangeOption}>
+                        <select name="dateRange" id="UserForm-dateRangeInput" className="UserForm-fieldInput" value={dateRangeOption} onChange={setDateRangeOption}>
                             <option value="this_year">This year</option>
                             <option value="last_year">Last year</option>
                             <option value="last_3">Last 3 months</option>
-                            <option value="last_12">Last 12 months</option>
-                            <option value="last_24">Last 2 years</option>
+                            <option value="last_12">Last 12 months</option> <option value="last_24">Last 2 years</option>
                             <option value="last_60">Last 5 years</option>
                         </select>
                     </div>
                     <div id="UserForm-userCookie" className="UserForm-field">
                         <label className="UserForm-label">NYT-S Cookie</label>
-                        <textarea className="UserForm-fieldInput" id="UserForm-userCookieInput" type="text" value={this.state.userCookie} onChange={this.setUserCookie} onKeyPress={this.handleKeyPress} ref={this.userCookieInputRef}/>
+                        <textarea className="UserForm-fieldInput" id="UserForm-userCookieInput" type="text" value={userCookie} onChange={setUserCookie} onKeyPress={handleKeyPress} ref={userCookieInputRef}/>
                     </div>
                 </div>
                 <input className="UserForm-submitButton" type="submit"/>
